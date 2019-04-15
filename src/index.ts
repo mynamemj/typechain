2bbbfcaf67b143e4b2c743672afdf0970eb79bb9
mynamemj <dlaudwp1313@gmail.com>
2019-04-16 02:11:34 +0900

    #2 Types in Typescript

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const sayHi = (name:string,age:number,gender:string): string =>{
+    return (`your name is ${name},and age is ${age} and you are ${gender}`);
+}
+console.log(sayHi("명제",24,'남자'));
+export {}
