3f174756718f5e33baf5bd10160592eb8efd826c
mynamemj <dlaudwp1313@gmail.com>
2019-04-16 16:29:53 +0900

    #3 Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
-interface Human {
-    name:string,
-    age:number,
-    gender:string
+// index,hash,previousHash,data,timestamp
+class Block{
+    public index:number;
+    public hash:string;
+    public previousHash:string;
+    public data:string;
+    public timestamp:number;
+    constructor(
+        index:number,
+        hash:string,
+        previousHash:string,
+        data:string,
+        timestamp:number,
+        ){
+        this.index=index;
+        this.hash=hash;
+        this.previousHash=previousHash;
+        this.data=data;
+        this.timestamp=timestamp;
+        }
 }
-
-const person={
-    name:"명제",
-    age:26,
-    gender:"멋진남자"
-}
-const sayHi = (person:Human): string =>{
-    return (`your name is ${person.name},and age is ${person.age} and you are ${person.gender}`);
-}
-console.log(sayHi(person));
+const mjBlock:Block=new Block(0,"202020202020","","Hello",123456);
+const chBlock:Block=new Block(1,"202022222202","202020202020","Hi",123457);
+let chain:[Block]=[mjBlock];
+chain.push(chBlock);
+console.log(chain);
 export {}
